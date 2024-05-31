3c86fa7552c820384bd4fb969812eeb5f943c7d9
kwrona05 <kwrona05@gmail.com>
2024-05-31 19:59:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const btn = document.querySelector('.print');
 const divTest = document.querySelector('.hide');
 const paragraph = document.querySelector('content')
-const ulList = document.querySelector('ul')
 
 function showDiv() {
     divTest.classList.toggle('show');
     divTest.classList.toggle('hide');
-    ulList.classList.toggle('show');
-    ulList.classList.toggle('hide');
 }
 
 btn.addEventListener('click', showDiv)
